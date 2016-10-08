e566aeb6b7215c3491a8879bde7cebc48ac5f9c0
Ryo Suzuki <ryosuzk@gmail.com>
2016-10-08 01:07:34 -0600

    Split main.cpp into view.h and view.cpp

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -2,7 +2,47 @@
 
 #include "viewer.h"
 
-Viewer::Viewer(bool fullscreen, bool deterministic)
-    : Screen(Vector2i(1280, 960), "Instant Meshes", true, fullscreen),
-      mOptimizer(mRes, true), mBVH(nullptr) {
-    resizeEvent(mSize);
+Viewer::Viewer() : Screen(Eigen::Vector2i(1024, 768), "CAD App") {
+  Window *window = new Window(this, "Viewer");
+  window->setPosition(Eigen::Vector2i(10, 10));
+  window->setLayout(new GroupLayout());
+
+  Eigen::MatrixXf indices(3, 2);
+  indices.col(0) << 0, 1, 2;
+  indices.col(1) << 2, 3, 0;
+
+  Eigen::MatrixXf positions(3, 4);
+  positions.col(0) << -1, -1, 0;
+  positions.col(1) <<  1, -1, 0;
+  positions.col(2) <<  1,  1, 0;
+  positions.col(3) << -1,  1, 0;
+
+  shader_.init(
+    /* An identifying name */
+    "a_simple_shader",
+
+    /* Vertex shader */
+    "#version 330\n"
+    "uniform mat4 modelViewProj;\n"
+    "in vec3 position;\n"
+    "void main() {\n"
+    "    gl_Position = modelViewProj * vec4(position, 1.0);\n"
+    "}",
+
+    /* Fragment shader */
+    "#version 330\n"
+    "out vec4 color;\n"
+    "uniform float intensity;\n"
+    "void main() {\n"
+    "    color = vec4(vec3(intensity), 1.0);\n"
+    "}"
+    );
+  shader_.bind();
+  shader_.uploadIndices(indices);
+  shader_.uploadAttrib("position", positions);
+  shader_.setUniform("intensity", 0.5f);
+}
+
+Viewer::~Viewer() {
+  shader_.free();
+}
--- src/viewer.h
+++ src/viewer.h
@@ -1,7 +1,7 @@
 
-#pragma once
-
-#include <set>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <nanogui/nanogui.h>
 
 using nanogui::Alignment;
 using nanogui::Arcball;
@@ -10,6 +10,7 @@ using nanogui::Button;
 using nanogui::CheckBox;
 using nanogui::Color;
 using nanogui::ComboBox;
+using nanogui::FormHelper;
 using nanogui::GLFramebuffer;
 using nanogui::GroupLayout;
 using nanogui::ImagePanel;
@@ -34,188 +35,12 @@ using nanogui::translate;
 using nanogui::unproject;
 using nanogui::utf8;
 
-struct CurvePoint;
-
 class Viewer : public Screen {
 public:
-    Viewer(bool fullscreen, bool deterministic);
-    virtual ~Viewer();
-
-    bool mouseMotionEvent(const Vector2i &p, const Vector2i &rel,
-                                  int button, int modifiers);
-
-    bool mouseButtonEvent(const Vector2i &p, int button, bool down,
-                                  int modifiers);
-
-    bool keyboardEvent(int key, int scancode, int action, int modifiers);
-
-    bool scrollEvent(const Vector2i &p, const Vector2f &rel);
-
-    void loadInput(std::string filename,
-                   Float creaseAngle = std::numeric_limits<Float>::infinity(),
-                   Float scale = -1, int face_count = -1, int vertex_count = -1,
-                   int rosy = 4, int posy = 4, int knn_points = 10);
-
-    void setSymmetry(int rosy, int posy);
-    void setExtrinsic(bool extrinsic);
-
-    void resetState();
-    void loadState(std::string filename, bool compat = false);
-    void saveState(std::string filename);
-    void renderMitsuba();
-    void setFloorPosition();
-    void draw(NVGcontext *ctx);
+  Viewer();
+  virtual ~Viewer();
 
 protected:
-    void extractMesh();
-    void extractConsensusGraph();
-
-    void drawContents();
-    void drawOverlay();
-
-    bool resizeEvent(const Vector2i &size);
-
-    void refreshColors();
-
-    void traceFlowLines();
-
-    void refreshStrokes();
-
-    void showProgress(const std::string &caption, Float value);
-
-    void computeCameraMatrices(Eigen::Matrix4f &model,
-                               Eigen::Matrix4f &view,
-                               Eigen::Matrix4f &proj);
-
-    void setLevel(int level);
-    void setTargetScale(Float scale);
-    void setTargetVertexCount(uint32_t v);
-    void setTargetVertexCountPrompt(uint32_t v);
-
-    bool createSmoothPath(const std::vector<Vector2i> &curve);
-
-    void repaint();
-
-    void setCreaseAnglePrompt(bool enabled, Float creaseAngle);
-    void shareGLBuffers();
-    bool refreshPositionSingularities();
-    bool refreshOrientationSingularities();
-    std::pair<Vector3f, Vector3f> singularityPositionAndNormal(uint32_t v) const;
-    bool toolActive() const;
-
-protected:
-    struct CameraParameters {
-        Arcball arcball;
-        float zoom = 1.0f, viewAngle = 45.0f;
-        float dnear = 0.05f, dfar = 100.0f;
-        Eigen::Vector3f eye = Eigen::Vector3f(0.0f, 0.0f, 5.0f);
-        Eigen::Vector3f center = Eigen::Vector3f(0.0f, 0.0f, 0.0f);
-        Eigen::Vector3f up = Eigen::Vector3f(0.0f, 1.0f, 5.0f);
-        Eigen::Vector3f modelTranslation = Eigen::Vector3f::Zero();
-        Eigen::Vector3f modelTranslation_start = Eigen::Vector3f::Zero();
-        float modelZoom = 1.0f;
-    };
-
-    std::vector<std::pair<int, std::string>> mExampleImages;
-    std::string mFilename;
-    bool mDeterministic;
-    bool mUseHalfFloats;
-
-    /* Data being processed */
-    std::map<uint32_t, uint32_t> mCreaseMap;
-    std::set<uint32_t> mCreaseSet;
-    VectorXb mNonmanifoldVertices;
-    VectorXb mBoundaryVertices;
-    MultiResolutionHierarchy mRes;
-    Optimizer mOptimizer;
-    BVH *mBVH;
-    MeshStats mMeshStats;
-    int mSelectedLevel;
-    Float mCreaseAngle;
-    Matrix4f mFloor;
-    VectorXu mE2E;
-
-    /* Painting tools */
-    std::vector<Vector2i> mScreenCurve;
-    std::vector<std::pair<uint32_t, std::vector<CurvePoint>>> mStrokes;
-
-    /* Extraction result */
-    MatrixXu mF_extracted;
-    MatrixXf mV_extracted;
-    MatrixXf mN_extracted, mNf_extracted;
-
-    /* Camera / navigation / misc */
-    CameraParameters mCamera;
-    CameraParameters mCameraSnapshots[12];
-    Vector2i mTranslateStart;
-    bool mTranslate, mDrag;
-    std::map<uint32_t, uint32_t> mOrientationSingularities;
-    std::map<uint32_t, Vector2i> mPositionSingularities;
-    bool mContinueWithPositions;
-
-    /* Colors */
-    Vector3f mSpecularColor, mBaseColor;
-    Vector3f mInteriorFactor, mEdgeFactor0;
-    Vector3f mEdgeFactor1, mEdgeFactor2;
-
-    /* OpenGL objects */
-    GLFramebuffer mFBO;
-    SerializableGLShader mPointShader63, mPointShader24, mPointShader44;
-    SerializableGLShader mMeshShader63, mMeshShader24, mMeshShader44;
-    SerializableGLShader mOrientationFieldShader;
-    SerializableGLShader mPositionFieldShader;
-    SerializableGLShader mPositionSingularityShader;
-    SerializableGLShader mOrientationSingularityShader;
-    SerializableGLShader mFlowLineShader, mStrokeShader;
-    SerializableGLShader mOutputMeshShader;
-    SerializableGLShader mOutputMeshWireframeShader;
-    bool mNeedsRepaint;
-    uint32_t mDrawIndex;
-
-    /* GUI-related */
-    enum Layers {
-        InputMesh,
-        InputMeshWireframe,
-        FaceLabels,
-        VertexLabels,
-        FlowLines,
-        OrientationField,
-        OrientationFieldSingularities,
-        PositionField,
-        PositionFieldSingularities,
-        BrushStrokes,
-        OutputMesh,
-        OutputMeshWireframe,
-        LayerCount
-    };
-
-    CheckBox *mLayers[LayerCount];
-    ComboBox *mVisualizeBox, *mSymmetryBox;
-    CheckBox *mExtrinsicBox, *mAlignToBoundariesBox;
-    CheckBox *mCreaseBox, *mPureQuadBox;
-    ProgressButton *mSolveOrientationBtn, *mSolvePositionBtn;
-    Button *mHierarchyMinusButton, *mHierarchyPlusButton;
-    Button *mSaveBtn, *mSwitchBtn;
-    PopupButton *mExportBtn;
-    ToolButton *mOrientationComb, *mOrientationAttractor, *mOrientationScareBrush;
-    ToolButton *mEdgeBrush, *mPositionAttractor, *mPositionScareBrush;
-    TextBox *mHierarchyLevelBox, *mScaleBox, *mCreaseAngleBox;
-    TextBox *mOrientationSingularityBox, *mPositionSingularityBox, *mSmoothBox;
-    Slider *mScaleSlider, *mCreaseAngleSlider, *mSmoothSlider;
-    Slider *mOrientationFieldSizeSlider, *mOrientationFieldSingSizeSlider;
-    Slider *mPositionFieldSingSizeSlider, *mFlowLineSlider;
-#ifdef VISUALIZE_ERROR
-    Graph *mGraph;
-#endif
-
-    /* Progress display */
-    std::function<void(const std::string &, Float)> mProgress;
-    Window *mProgressWindow;
-    ProgressBar *mProgressBar;
-    Label *mProgressLabel;
-    tbb::spin_mutex mProgressMutex;
-    double mLastProgressMessage;
-    double mOperationStart;
-    uint32_t mOutputMeshFaces, mOutputMeshLines;
-    uint32_t mFlowLineFaces, mStrokeFaces;
+  nanogui::GLShader shader_;
 };
+
